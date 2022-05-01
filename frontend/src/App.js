b76434c47cbc7952789065733af3d98f18d32c0d
kiran.sadvilkar <kiran.sadvilkar93@gmail.com>
2022-05-01 15:47:29 +0530

    Bootstrap NavBar added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
     <>
-      <h1>Welcome to Ekart</h1>
+      <Header />
+
+      <main className='py-3'>
+        <Container>
+          <h1>Welcome to Ekart</h1>
+        </Container>
+      </main>
+
+      <Footer />
     </>
-  );
+  )
 }
 
-export default App;
+export default App
