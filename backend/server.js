2e75e8041e674ecd9fbb3528d4e55ce8715a304a
kiran.sadvilkar <kiran.sadvilkar93@gmail.com>
2022-05-03 01:16:04 +0530

    MongoDB connect added, DB schema and seeders added

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,33 @@
 import express from 'express'
-import products from './data/products.js'
+import connectDB from './config/db.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
+import colors from 'colors'
 import dotenv from 'dotenv'
 
+import productRoutes from './routes/productRoutes.js'
+
 dotenv.config()
+
+connectDB()
+
 const app = express()
 
 app.get('/', (req, res) => {
   res.send('API is running')
 })
 
-app.get('/api/products', (req, res) => {
-  res.json(products)
-})
+app.use('/api/products', productRoutes)
 
-app.get('/api/product/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id)
-  res.json(product)
-})
+app.use(notFound)
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
 app.listen(
   5000,
-  console.log(`server running in ${process.env.NODE_ENV} on port ${PORT}`)
+  console.log(
+    `server running in ${process.env.NODE_ENV} on port ${PORT}`.green.bold
+      .underline
+  )
 )
